8f53e875262e3fe25b38266276f97b5fffdb4e50
Marc Boudou <squall2022@gmail.com>
2023-06-01 13:54:45 +0200

    Feature/Cache key improvements (#1)
    * remove Nimble (temp)
    
    * update dependencies
    
    * move dep
    
    * update resolved
    
    * format
    
    * change key to MD5
    
    * remove macos
    
    * ios 13
    
    * cache filename independent from resolution
    
    * cacheKey public
    
    * open
    
    * open saveCacheData
    
    * revert saveCacheData
    
    * add transformUrlForCacheKey
    
    * remove configure
    
    * cacheKeyHandler
    
    * add ?
    
    * cache regex

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
-// swift-tools-version:5.1
+// swift-tools-version: 5.7
 
 import PackageDescription
 
 let package = Package(
   name: "HLSCachingReverseProxyServer",
   platforms: [
-    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9)
+    .iOS(.v13)
   ],
   products: [
     .library(name: "HLSCachingReverseProxyServer", targets: ["HLSCachingReverseProxyServer"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "9.0.0")),
+    .package(url: "https://github.com/SlaunchaMan/GCDWebServer.git", branch: "swift-package-manager"),
+    .package(url: "https://github.com/pinterest/PINCache.git", .upToNextMinor(from: "3.0.0")),
     .package(url: "https://github.com/devxoul/SafeCollection.git", .upToNextMajor(from: "3.1.0")),
   ],
   targets: [
-    .target(name: "HLSCachingReverseProxyServer"),
-    .testTarget(name: "HLSCachingReverseProxyServerTests", dependencies: ["HLSCachingReverseProxyServer", "Nimble", "SafeCollection"]),
+    .target(name: "HLSCachingReverseProxyServer", dependencies: ["GCDWebServer", "PINCache"]),
+    .testTarget(name: "HLSCachingReverseProxyServerTests", dependencies: ["HLSCachingReverseProxyServer", "SafeCollection"]),
   ]
 )
